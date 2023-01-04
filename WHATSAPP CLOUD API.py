f7c0953006463ec33b00c7a22195a1c7b4148ddf
Matus18 <106945625+Matus18@users.noreply.github.com>
2023-01-04 00:05:23 -0300

    Update and rename META API.py to WHATSAPP CLOUD API.py

--- WHATSAPP CLOUD API.py
+++ WHATSAPP CLOUD API.py
@@ -1,29 +1,29 @@
 import requests
 
-ACCESS_TOKEN = "EAAL028cN0asBAIVwc744AODZBOD8CmaoAeklEk71ZC17qyy7vG2mM93tpZC3KNXhaEwldkof85CYpowbrMn6Scpmnhl1cs71l6QTbAJf7FqaXI4ZBR8T84qB18pU1WzOF5KIwOJ8VxEEWrcHKGyDBr00KQqNZBARbL8UJZAf5giZCIDyzh30JUKYcfNGpZBNg3HKAjxL3cStFgZDZD"
-PHONE_NUMBER = "569"
-MESSAGE = "Hola, ¿cómo estás?"
-url = f"https://graph.facebook.com/v15.0/110002441945576/messages?access_token={ACCESS_TOKEN}"
+ACCESS_TOKEN = "API KEY"
+PHONE_NUMBER = "CODIGO DE PAIS + NUMERO"
+MESSAGE = "MENSAJE"
+url = f"https://graph.facebook.com/v15.0/ID_URL/messages?access_token={ACCESS_TOKEN}"
 
 # JSON API WHATSAPP
 payload = { "messaging_product": "whatsapp",
                 "to": PHONE_NUMBER,
                 "type": "template",
                 "template": {
-                    "name": "pedido_enviado_sinpago",
+                    "name": "NOMBRE DE PLANTILLA DE MENSAJE",
                     "language": {
-                        "code": "es"
+                        "code": "IDIOMA"
                     },
                     "components": [{
                         "type": "body",
                         "parameters": [
                             {
                                 "type": "text",
-                                "text": "Benjamin"
+                                "text": "INGRESA TEXTO"
                             },
                             {
                               "type":"text",
-                              "text": "$23.990"
+                              "text": "INGRESA TEXTO"
                             }
                         ]
                 }]
